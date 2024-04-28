fac74590b9fc89ac1bad26d9b44805b6fab78b01
Mohamad-H-Hallal <hallalmohamad49@gmail.com>
2024-04-29 00:43:27 +0300

    .

--- app/src/main/java/com/example/project/ManagePartsActivity.java
+++ app/src/main/java/com/example/project/ManagePartsActivity.java
@@ -1,5 +1,7 @@
 package com.example.project;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -13,16 +15,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class ManagePartsActivity extends BaseActivity {
     private ImageButton backButton;
+    private FloatingActionButton fab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.themeColor));
         setContentView(R.layout.activity_manage_parts);
         backButton = findViewById(R.id.back_arrow2);
-
-
+        fab = findViewById(R.id.fab);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         String selectedLanguage = preferences.getString("selected_language", "");
         if (selectedLanguage.equals("en")) {
@@ -32,12 +37,16 @@ public class ManagePartsActivity extends BaseActivity {
 
             backButton.setImageResource(R.drawable.ic_back_ar);
         }
-
-
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ManagePartsActivity.this, AddPartsActivity.class));
             }
         });
     }
