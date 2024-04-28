cdb5ce07bef9a4daccf07f1a8708f9b6b2464273
Mohamad-H-Hallal <hallalmohamad49@gmail.com>
2024-04-29 01:15:19 +0300

    .

--- app/src/main/java/com/example/project/AddPartsActivity.java
+++ app/src/main/java/com/example/project/AddPartsActivity.java
@@ -3,6 +3,7 @@ package com.example.project;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,17 +14,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class AddPartsActivity extends BaseActivity {
 
+    private ImageButton back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_parts);
         getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.themeColor));
+        back = findViewById(R.id.back_arrow7);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         String selectedLanguage = preferences.getString("selected_language", "");
         if (selectedLanguage.equals("en")) {
-//            back.setImageResource(R.drawable.ic_back_en);
+            back.setImageResource(R.drawable.ic_back_en);
         } else if (selectedLanguage.equals("ar")) {
-//            back.setImageResource(R.drawable.ic_back_ar);
+            back.setImageResource(R.drawable.ic_back_ar);
         }
     }
 }
