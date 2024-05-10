ccfc3b159fbdb1113ca55379e9192c86021a7c81
Mohamad-H-Hallal <hallalmohamad49@gmail.com>
2024-05-10 10:20:21 +0300

    .

--- app/src/main/java/com/example/project/PartsAdapter.java
+++ app/src/main/java/com/example/project/PartsAdapter.java
@@ -7,8 +7,6 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-import androidx.appcompat.widget.AppCompatButton;
-
 import com.google.android.material.imageview.ShapeableImageView;
 
 import org.json.JSONArray;
