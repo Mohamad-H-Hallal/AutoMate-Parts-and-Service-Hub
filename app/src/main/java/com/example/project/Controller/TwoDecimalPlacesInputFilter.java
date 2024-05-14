7d147c378a6b84c83b42135a182b0703e1b75fc6
Mohamad-H-Hallal <hallalmohamad49@gmail.com>
2024-05-14 16:40:33 +0300

    .

--- app/src/main/java/com/example/project/Controller/TwoDecimalPlacesInputFilter.java
+++ app/src/main/java/com/example/project/Controller/TwoDecimalPlacesInputFilter.java
@@ -7,15 +7,20 @@ public class TwoDecimalPlacesInputFilter implements InputFilter {
 
     @Override
     public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+
         if (source.length() == 0) {
             return null;
         }
+
         char firstChar = source.charAt(0);
-        if (firstChar == '-' && dest.length() == 0) {
-            return "";
-        } else if (!Character.isDigit(firstChar) && firstChar != '.') {
+        if (firstChar == '-') {
             return "";
         }
+
+        if (!Character.isDigit(firstChar) && firstChar != '.') {
+            return "";
+        }
+
         int dotPos = -1;
         int length = dest.length();
         for (int i = 0; i < length; ++i) {
@@ -24,7 +29,7 @@ public class TwoDecimalPlacesInputFilter implements InputFilter {
                 break;
             }
         }
-        if (dotPos >= 0 && dest.length() - dotPos > 3) {
+        if (dotPos >= 0 && dest.length() - dotPos > 2) {
             return "";
         }
         return null;
