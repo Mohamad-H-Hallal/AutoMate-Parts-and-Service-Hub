0409899f384d6dd6bfc5ebbdeb4661dc53aa83af
Mohamad-H-Hallal <hallalmohamad49@gmail.com>
2024-05-16 11:50:52 +0300

    .

--- app/src/main/java/com/example/project/View/Activities/ViewPartsActivity.java
+++ app/src/main/java/com/example/project/View/Activities/ViewPartsActivity.java
@@ -1,20 +1,31 @@
 package com.example.project.View.Activities;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import com.example.project.Controller.PartController;
+import com.example.project.Model.PartModel;
 import com.example.project.R;
+import com.example.project.View.Adapters.ManagePartsAdapter;
+import com.example.project.View.Adapters.PartsAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewPartsActivity extends BaseActivity {
     ImageButton backButton;
     ListView lv;
+    String subcategory,category;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +34,23 @@ public class ViewPartsActivity extends BaseActivity {
         backButton = findViewById(R.id.back_arrow5);
         lv = findViewById(R.id.v_parts_list);
 
-//        PartsAdapter adapter = new PartsAdapter(this, Parts.getParts());
+        Intent intent = getIntent();
+        subcategory = intent.getStringExtra("subcategory");
+        category = intent.getStringExtra("category");
+
+        PartController partCont = new PartController();
+        partCont.manageParts(this, subcategory, new PartController.PartManageListener() {
+            @Override
+            public void onPartsManage(List<PartModel> parts, ArrayList<String> imagePaths) {
+                PartsAdapter adapter = new PartsAdapter(ViewPartsActivity.this, parts, imagePaths);
+                lv.setAdapter(adapter);
+            }
+
+            @Override
+            public void onError(String error) {
+                Toast.makeText(ViewPartsActivity.this, error, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         String selectedLanguage = preferences.getString("selected_language", "");
