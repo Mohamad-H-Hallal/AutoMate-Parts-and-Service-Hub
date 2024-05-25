7e2eb9805b37539921857f8ae2d03856da2f5340
Mohamad-H-Hallal <hallalmohamad49@gmail.com>
2024-05-25 23:58:03 +0300

    .

--- app/src/main/java/com/example/project/View/Fragments/DiagnosticsFragment.java
+++ app/src/main/java/com/example/project/View/Fragments/DiagnosticsFragment.java
@@ -93,7 +93,7 @@ public class DiagnosticsFragment extends BaseFragment implements SerialListener 
     private ListView dataListView;
     private static final int PICK_FILE_REQUEST_CODE = 1;
     private AlertDialog Dialog;
-    private TextView dataClearAll;
+    private TextView dataClearAll, noData;
     private SerialService service;
 
 
@@ -128,6 +128,7 @@ public class DiagnosticsFragment extends BaseFragment implements SerialListener 
         dataListView = view.findViewById(R.id.dataListView);
         infoData = view.findViewById(R.id.infoData);
         dataClearAll = view.findViewById(R.id.dataClearAll);
+        noData = view.findViewById(R.id.noData);
         service = new SerialService();
 
 //        ipServerInput.addTextChangedListener(new TextWatcher() {
@@ -503,7 +504,10 @@ public class DiagnosticsFragment extends BaseFragment implements SerialListener 
             @Override
             public void onResponse(Call<UserDataResponse> call, Response<UserDataResponse> response) {
                 if (response.body() != null && response.body().isHasData()) {
+                    noData.setVisibility(View.GONE);
                     fetchRecentFiles(UserData.getId());
+                } else {
+                    noData.setVisibility(View.VISIBLE);
                 }
             }
 
